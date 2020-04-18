96b5f7287fb56f4f35da79ea0a79334053c8b3d9
Kristi Kacaj <kristik.web@gmail.com>
2020-04-18 16:50:21 -0400

    placed event key listener and if else statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 let wins = 0;
 let losses = 0;
-let attempts = 10;
+let guessesLeft = 9;
 let lettersGuessed = [];
-let ranLetter = ranLetter;
+let ranLetter = '';
 let letters = "abcdefghijklmnopqrstuvwxyz"
 
 ranLetter = letters[Math.floor(Math.random() * letters.length)];
@@ -11,4 +11,38 @@ console.log(ranLetter);
 function userGuess() {
     ranLetter = letters[Math.floor(Math.random() * letters.length)];
     console.log(ranLetter);
-}
+};
+
+document.onkeyup = function (event) {
+    let playersGuess = event.key;
+
+    if (playersGuess === ranLetter) {
+        wins++;
+        guessesLeft = 9;
+        lettersGuessed = [];
+    } 
+
+    userGuess ();
+    if (playerGuess !== ranLetter) {
+        guessesLeft--;
+    }
+
+    if (guessesLeft == 0) {
+        losses++;
+        lettersGuessed = [];
+        guessesLeft = 9;
+    }
+
+    if (lettersGuessed.indexOf(playerGuess) >= 0) {
+
+    } else {
+        lettersGuessed.push(playerGuess);
+        document.getElementById('playerGuess').innerHTML = lettersGuessed;
+        console.log(lettersGuessed);
+    }
+    
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('losses').innerHTML = losses;
+    document.getElementById('guessesLeft').innerHTML = guessesLeft;
+
+};
