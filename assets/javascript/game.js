9cc90766453cae9c7bab7d75654ce087aae66419
Kristi Kacaj <kristik.web@gmail.com>
2020-04-18 16:20:07 -0400

    used math methods to select random letter from array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,5 +2,13 @@ let wins = 0;
 let losses = 0;
 let attempts = 10;
 let lettersGuessed = [];
-let randomLetter = randomLetter;
+let ranLetter = ranLetter;
 let letters = "abcdefghijklmnopqrstuvwxyz"
+
+ranLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(ranLetter);
+
+function userGuess() {
+    ranLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(ranLetter);
+}
